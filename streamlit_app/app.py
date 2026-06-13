7aa740f8628799ee54dd5ae92202ddfc8877a749
Tobi-DataDetective <folayantobitaiwo@gmail.com>
2026-06-13 17:08:54 -0400

    function rollup

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -10,5 +10,5 @@ rodeo_planning        = st.Page("pages/rodeo_planning.py",        title="Rodeo P
 database_update       = st.Page("pages/database_update.py",       title="Database Update")
 function_rollup       = st.Page("pages/function_rollup.py",       title="Function Rollup")
 
-pg = st.navigation([pick_planning, attendance_recon, voice_pick, equipment_utilization, rodeo_planning, database_update, function_rollup])
+pg = st.navigation([pick_planning, attendance_recon, voice_pick, equipment_utilization, rodeo_planning, function_rollup, database_update])
 pg.run()
